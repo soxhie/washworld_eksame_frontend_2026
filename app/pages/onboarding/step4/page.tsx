5256b287f18cbdb0b1041c0376f02a747e7aca15
SophiaAnina <Sophia.anina@gmail.com>
2026-05-22 14:05:16 +0200

    card / mobilepay added

--- app/pages/onboarding/step4/page.tsx
+++ app/pages/onboarding/step4/page.tsx
@@ -4,25 +4,25 @@ import { useRouter } from "next/navigation";
 import { getOnboardingData, saveOnboardingData, clearOnboardingData } from "../utils/onboardingStorage";
 import { FaArrowRight, FaChevronLeft } from "react-icons/fa";
 import { CiCreditCard1 } from "react-icons/ci";
-
+ 
 import CardInput from "../components/cardInput";
 import "../onboarding.css";
 import MobilePayInput from "../components/MobilepayInput";
-
+ 
 type PaymentMethod = {
     payment_gateway_id: string;
     payment_gateway_name: string;
     payment_gateway_icon_path: string;
 };
-
+ 
 export default function OnboardingStep4() {
     const router = useRouter();
     const [paymentMethod, setPaymentMethod] = useState("Card");
     const [methods, setMethods] = useState<PaymentMethod[]>([]);
-
+    
     const [submitting, setSubmitting] = useState(false);
     const [error, setError] = useState("");
-
+ 
     useEffect(() => {
         fetch("http://localhost:80/api-payment-gateways")
             .then(res => res.json())
@@ -35,12 +35,12 @@ export default function OnboardingStep4() {
     }, []);
     const payload = getOnboardingData();
     console.log("payload being sent:", payload);
-
+ 
     const handleSubmit = async () => {
         if (!paymentMethod) { setError("Vælg venligst en betalingsmetode."); return; }
         saveOnboardingData({ transaction_gateway_fk: paymentMethod });
         const payload = getOnboardingData();
-
+ 
         setSubmitting(true);
         try {
             const res = await fetch("http://localhost:80/api-signup", {
@@ -58,14 +58,14 @@ export default function OnboardingStep4() {
             setSubmitting(false);
         }
     };
-
+ 
     return (
         <div className="Onboarding-4">
             <button className='tilbageLink' type="button" onClick={() => router.back()}>
                 <FaChevronLeft /> Tilbage
             </button>
             <h1>Betalingsmetode</h1>
-
+ 
             {methods.map(method => (
                 <div
                     key={method.payment_gateway_id}
@@ -85,16 +85,31 @@ export default function OnboardingStep4() {
                         checked={paymentMethod === method.payment_gateway_id}
                         readOnly
                     />
+                    
                     <CiCreditCard1 size={39} />
                     <h3>{method.payment_gateway_name}</h3>
                 </div>
             ))}
 
-            {paymentMethod === "card" && <CardInput />}
+               
+                {(() => {
+                    const selectedMethod = methods.find(m => m.payment_gateway_id === paymentMethod);
+                    if (!selectedMethod) return null;
+                    if (selectedMethod.payment_gateway_name === "Mobilepay") {
+                        return <MobilePayInput />;
+                    }
+                    if (selectedMethod.payment_gateway_name === "Betalingskort") {
+                        return <CardInput />;
+                    }
+                    return null;
+                })()}
+                  
 
             <button className='nextButton' type="button" onClick={handleSubmit} disabled={submitting}>
-                {submitting ? "Sender..." : <FaArrowRight />}
+                 <FaArrowRight />
             </button>
         </div>
     );
 }
+ 
+ 
