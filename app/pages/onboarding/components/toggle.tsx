c622fd4b57b8797c2a2f72f3d1f322326bac7ed6
SophiaAnina <Sophia.anina@gmail.com>
2026-06-08 11:16:33 +0200

    toggle commit

--- app/pages/onboarding/components/toggle.tsx
+++ app/pages/onboarding/components/toggle.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../onboarding.css"
 
 type ToggleProps = {
@@ -44,51 +43,4 @@ export default function Toggle({
             {error && <div style={{ color: "red", marginTop: 4 }}>{error}</div>}
         </div>
     )
-=======
-import "../onboarding.css"
-
-type ToggleProps = {
-    termsAccepted: boolean;
-    privacyAccepted: boolean;
-    onTermsChange: (checked: boolean) => void;
-    onPrivacyChange: (checked: boolean) => void;
-    error?: string;
-}
-
-export default function Toggle({
-    termsAccepted,
-    privacyAccepted,
-    onTermsChange,
-    onPrivacyChange,
-    error,
-}: ToggleProps){
-
-    return(
-        <div>
-            <div className="toggleContainer">
-                <p>Jeg acceptere <a href="">Terms & Condtions</a></p>
-                <label className="switch">
-                    <input
-                        type="checkbox"
-                        checked={termsAccepted}
-                        onChange={(e) => onTermsChange(e.target.checked)}
-                    />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-            <div className="toggleContainer">
-                <p>Jeg acceptere <a href="">Privacy guidelines</a></p>
-                <label className="switch">
-                    <input
-                        type="checkbox"
-                        checked={privacyAccepted}
-                        onChange={(e) => onPrivacyChange(e.target.checked)}
-                    />
-                    <span className="slider round"></span>
-                </label>
-            </div>
-            {error && <div style={{ color: "red", marginTop: 4 }}>{error}</div>}
-        </div>
-    )
->>>>>>> 97e98603ff2068e4df35af26b4072051b1596103
 }
